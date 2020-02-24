036f0f37bb793593ed51075e495392e419335354
Joe <ja582@njit.edu>
2020-02-23 22:52:32 -0500

    production displaying functionality is running

--- static/productList.js
+++ static/productList.js
@@ -1,48 +1,55 @@
 const mockDatabase = [
     {
-        img: '1.jpg',
+        img: 'imgs/1.jpg',
         name: 'Moving Pictures',
-        Artist: 'Rush',
-        Year: '1981',
-        price: '10',
+        artist: 'Rush',
+        year: '1981',
+        price: '5',
         categories: ['Progressive Rock', '80s'],
     },
     {
-        img: '1.jpg',
+        img: 'imgs/2.jpg',
         name: 'Hemispheres',
-        Artist: 'Rush',
-        Year: '1978',
-        price: '10',
+        artist: 'Rush',
+        year: '1978',
+        price: '41',
         categories: ['Progressive Rock', '70s'],
     },
     {
-        img: '1.jpg',
+        img: 'imgs/3.jpg',
         name: 'Permanent Waves',
-        Artist: 'Rush',
-        Year: '1980',
-        price: '10',
+        artist: 'Rush',
+        year: '1980',
+        price: '23',
         categories: ['Progressive Rock', '80s'],
     },
     {
-        img: '1.jpg',
+        img: 'imgs/1.jpg',
         name: 'Roll the Bones',
-        Artist: 'Rush',
-        Year: '1981',
-        price: '10',
+        artist: 'Rush',
+        year: '1981',
+        price: '60',
         categories: ['Progressive Rock', 'Hard Rock', '90s'],
     },
     {
-        img: '1.jpg',
+        img: 'imgs/1.jpg',
         name: 'Truth',
-        Artist: 'Jeff Beck',
-        Year: '1968',
-        price: '10',
+        artist: 'Jeff Beck',
+        year: '1968',
+        price: '89',
         categories: ['Blues Rock', 'Hard Rock', '70s'],
     },
-
 ];
 
-function Products(){
-
-
-}
+function renderData(results){
+        const cardHolder = document.querySelector('.card-deck');
+        window.cardHolder = cardHolder;
+        cardHolder.innerHTML = '';
+        const cardRows = results.map(function (result) {
+            return '<div class="card"><div class="card-body"><img class="card-img-top" src=' + result.img + '><h5 class="card-title">' + result.name + '</h5><p class="card-text">' + result.artist + '</p><p class="card-text">' + result.year + '</p><p class="card-text">' + result.price + '</p></div></div>';
+        });
+        cardRows.forEach(function (row) {
+            cardHolder.innerHTML += row;
+        });
+    }
+    renderData(mockDatabase);
