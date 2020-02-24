182db35ca1416457b759299cdf87260c22951d26
Joe <ja582@njit.edu>
2020-02-23 23:59:23 -0500

    Working on sorting functionality

--- static/productList.js
+++ static/productList.js
@@ -1,6 +1,6 @@
 const mockDatabase = [
     {
-        img: 'imgs/1.jpg',
+        img: 'imgs/Moving_Pictures.jpg',
         name: 'Moving Pictures',
         artist: 'Rush',
         year: '1981',
@@ -8,7 +8,7 @@ const mockDatabase = [
         categories: ['Progressive Rock', '80s'],
     },
     {
-        img: 'imgs/2.jpg',
+        img: 'imgs/Rush_Hemispheres.jpg',
         name: 'Hemispheres',
         artist: 'Rush',
         year: '1978',
@@ -16,7 +16,7 @@ const mockDatabase = [
         categories: ['Progressive Rock', '70s'],
     },
     {
-        img: 'imgs/3.jpg',
+        img: 'imgs/Rush_Permanent_Waves.jpg',
         name: 'Permanent Waves',
         artist: 'Rush',
         year: '1980',
@@ -24,7 +24,7 @@ const mockDatabase = [
         categories: ['Progressive Rock', '80s'],
     },
     {
-        img: 'imgs/1.jpg',
+        img: 'imgs/Rush_roll_the_bones.jpg',
         name: 'Roll the Bones',
         artist: 'Rush',
         year: '1981',
@@ -32,9 +32,9 @@ const mockDatabase = [
         categories: ['Progressive Rock', 'Hard Rock', '90s'],
     },
     {
-        img: 'imgs/1.jpg',
+        img: 'imgs/Jeff_Beck-Truth.jpg',
         name: 'Truth',
-        artist: 'Jeff Beck',
+        artist: 'Beatles',
         year: '1968',
         price: '89',
         categories: ['Blues Rock', 'Hard Rock', '70s'],
@@ -46,10 +46,20 @@ function renderData(results){
         window.cardHolder = cardHolder;
         cardHolder.innerHTML = '';
         const cardRows = results.map(function (result) {
-            return '<div class="card"><div class="card-body"><img class="card-img-top" src=' + result.img + '><h5 class="card-title">' + result.name + '</h5><p class="card-text">' + result.artist + '</p><p class="card-text">' + result.year + '</p><p class="card-text">' + result.price + '</p></div></div>';
+            return '<div class="card"><div class="card-body"><img class="card-img-top" src=' + result.img + '><h5 class="card-title">' + result.name + '</h5><p class="card-text">' + result.artist + '</p><p class="card-text">' + result.year + '</p><p class="card-text">$' + result.price + '</p></div></div>';
         });
         cardRows.forEach(function (row) {
             cardHolder.innerHTML += row;
         });
     }
     renderData(mockDatabase);
+
+function showArtists(artistName){
+    var artists = mockDatabase.filter(function(e){
+        return e.artist = artistName;
+    })
+    renderData(showArtists);
+    console.log(artists);
+}
+
+
